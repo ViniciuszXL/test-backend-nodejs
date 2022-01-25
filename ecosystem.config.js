62b7447abbf132e813d29599ae949b0e4e72d1b8
Marcus Mendonça <pessoal.marcusvinicius@outlook.com.br>
2022-01-25 16:25:53 -0400

    Remoção de dependências Types

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [{
         name: "catalog-rest-api",
-        script: "./dist/main.js",
+        script: "main.js",
         instances: 0,
         exec_mode: "cluster"
     }]
